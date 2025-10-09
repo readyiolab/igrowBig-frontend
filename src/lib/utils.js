9a35dea7461b721862442e78e0bf79d9482097cc
readyiolab <github@arbilo.com>
2025-10-09 14:46:28 +0530

    chage in ui

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -4,3 +4,12 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
+
+
+export function formatPrice(price) {
+  if (isNaN(price)) return "₹0.00";
+  return new Intl.NumberFormat("en-IN", {
+    style: "currency",
+    currency: "INR",
+  }).format(price);
+}
